08dfc5edcfcf041e316da8dec4404f707716f40b
Daniel Buriticá <dani.ferbu@gmail.com>
2022-02-20 23:48:49 -0500

    Configure routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Game from "./components/Game/Game";
+import NavbarTriqui from "./components/NavbarTriqui/NavbarTriqui";
+import HistoryTable from "./components/History/History";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <NavbarTriqui />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="game" element={<Game />} />
+        <Route path="history" element={<HistoryTable />} />
+      </Routes>
     </div>
   );
 }
