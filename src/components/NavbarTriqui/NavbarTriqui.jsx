5b2fbad4258247d2e9e751d188fcd628b5d8ba5b
Daniel Buriticá <dani.ferbu@gmail.com>
2022-02-20 23:47:48 -0500

    Create Navbar component

---
+++ src/components/NavbarTriqui/NavbarTriqui.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import styles from "./NavbarTriqui.module.css";
+
+export default function NavbarTriqui() {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={styles.colorNav}
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/logo.png"
+              width="35"
+              height="35"
+              className="d-inline-block align-top"
+            />
+            <span className={styles.textBrand}>Triqui</span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className={styles.itemNavbar} href="/">
+                Inicio
+              </Nav.Link>
+              <Nav.Link className={styles.itemNavbar} href="/history">
+                Historial
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
