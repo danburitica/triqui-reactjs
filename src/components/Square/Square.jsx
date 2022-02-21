01f868c92f0699b83cf0b06ddaf1c3eb37c0a05d
Daniel Buriticá <dani.ferbu@gmail.com>
2022-02-20 23:47:23 -0500

    Create Square component

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+import styles from "./Square.module.css";
+import x from "../../assets/X.png";
+import o from "../../assets/O.png";
+
+export default function Square({ value, turn, winnerSquare, onClick }) {
+  const onClickSquare = () => {
+    if (turn !== null && value === null) onClick();
+  };
+
+  return (
+    <Col>
+      <button className={styles.square} onClick={onClickSquare}>
+        {value === "x" && (
+          <img
+            src={x}
+            className={winnerSquare ? styles.winnerAnimation : ""}
+            alt="X Symbol"
+          />
+        )}
+        {value === "o" && (
+          <img
+            src={o}
+            className={winnerSquare ? styles.winnerAnimation : ""}
+            alt="O Symbol"
+          />
+        )}
+      </button>
+    </Col>
+  );
+}
